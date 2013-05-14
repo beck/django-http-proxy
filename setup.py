2a6de90334112f8bd52274e76e9c6bc7b117101d
Doug Beck <doug@douglasbeck.com>
2013-05-14 15:03:34 -0700

    Cleanup and simplify install

--- setup.py
+++ setup.py
@@ -1,31 +1,15 @@
-from distribute_setup import use_setuptools; use_setuptools()
-
-from setuptools import setup, find_packages
-
-#import os.path
-#import distutils
-#from distutils.core import setup
-
+#!/usr/bin/env python
+from distutils.core import setup
 from httpproxy import __version__
-
-#here = os.path.dirname(os.path.abspath(__file__))
-
-# Get long_description from index.txt
-#f = open(os.path.join(here, 'docs', 'index.txt'))
-#long_description = f.read().strip()
-#long_description = long_description.split('split here', 1)[1]
-#f.close()
 
 setup(
     name='django-http-proxy',
     version=__version__,
     description='A simple HTTP proxy for the Django framework.',
-    #long_description=long_description,
+    url='https://github.com/beck/django-http-proxy',
     author='Yuri van der Meer',
     author_email='django-http-proxy@yvandermeer.net',
-    url='http://httpproxy.yvandermeer.net/',
-    download_url='http://bitbucket.org/yvandermeer/django-http-proxy/get/v%s.zip' % __version__,
-    packages=find_packages(),
+    packages=['httpproxy'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
